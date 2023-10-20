099993ff8d16848fcc4007d25bfe9bd9710508fb
chart099 <122659980+chart099@users.noreply.github.com>
2023-10-19 19:39:38 -0500

    worked on server.js

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const api = require('./public/assets/js/index.js');
+
+const PORT = process.env.port || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
