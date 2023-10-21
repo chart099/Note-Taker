5afcabdc1ffdb2df919595487dd9699c607e7ff3
chart099 <122659980+chart099@users.noreply.github.com>
2023-10-21 13:55:43 -0500

    added delete function

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,5 @@
 const app = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
 
@@ -7,24 +7,40 @@ app.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
-  app.post('/', (req, res) => {
-    console.log(req.body);
-  
-    const { title, text } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text,
-        id: uuidv4(),
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(`Note added successfully`);
-    } else {
-      res.error('Error in adding note');
-    }
-  });
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+app.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      
+      const result = json.filter((note) => note.id !== noteId);
+
+      
+      writeToFile('./db/db.json', result);
+
+      
+      res.json(`Item ${noteId} has been deleted `);
+    });
+});
 
 
   module.exports = app;
